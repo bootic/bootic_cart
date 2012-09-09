bff78042f5d4c80c1969bbc0fefecec1c5d7ce58
Ismael Celis <ismaelct@gmail.com>
2012-09-09 17:15:57 +0100

    Annotated source.

--- src/bootic_cart.js
+++ src/bootic_cart.js
@@ -1,8 +1,22 @@
+// Cart private constructor
+// =========================
 var Cart = function () {
   this.reset(true)
 }
 
 Cart.prototype = {
+  
+  // Load server cart data
+  // =====================
+  // 
+  //  Takes:
+  //
+  // - `fn`: a callback to be run once the server has responded successfully
+  //
+  // Triggers:
+  //
+  // - `loading`: before server response
+  // - `loaded`: after successfully loaded from server. Updated cart object passed as argument.
   
   load: function (fn) {
     this.trigger('loading')
@@ -15,6 +29,11 @@ Cart.prototype = {
     return this
   },
   
+  // Reset client-side cart
+  // =======================
+  // 
+  // Resets product list and totals. Called on instantiation and useful for testing.
+  
   reset: function (silent) {
     this.products = []
     this.units = 0
@@ -26,6 +45,28 @@ Cart.prototype = {
     return this
   },
   
+  // Add a product to the cart
+  // ==========================
+  //
+  // Increments quantity if product already in the cart
+  //
+  // Takes:
+  //
+  // - `productId`: product ID to ad to cart
+  // - `fn`: (optional) a callback to be run with the loaded cart as an argument.
+  // - `opts`: (optional) options object with default overrides.
+  //
+  // Options are:
+  //
+  // - `url`: URL to load cart from (default: /cart/cart_items)
+  // - `type`: request method (default: 'get')
+  // - `qty`: quantity to increment product units in the cart by (default: 1)
+  //
+  // Triggers:
+  //
+  // - `adding`: before server response
+  // - `added`: after successfully added to server. Added product passed as argument.
+   
   add: function (productId, fn, opts) {
     fn = fn || $.noop;
     
@@ -33,7 +74,7 @@ Cart.prototype = {
       url: '/cart/cart_items',
       qty: 1,
       type: 'post',
-      dataType: 'json',
+      dataType: 'json'
     }, opts || {})
     
     var data = {
@@ -63,7 +104,9 @@ Cart.prototype = {
     return this
   },
   
-  // Sync. Pass a function for async
+  // Find a product by product ID
+  // ============================
+  
   find: function (productId, fn) {
     var match = null;
     this.forEach(function (item) {
@@ -86,6 +129,25 @@ Cart.prototype = {
       }
     })
   },
+  
+  // Remove a product by product id
+  // ===============================
+  //
+  // Takes:
+  //
+  // - `productId`: product ID to remove from cart
+  // - `fn`: (optional) callback to be run after successful removal. Removed product passed as argument.
+  // - `opts`: (optional) options object
+  //
+  // Options are:
+  //
+  // - `url`: URL to DELETE (default: /cart/cart_items/:cart_item_id)
+  // - `type`: request method (default: 'delete')
+  //
+  // Triggers:
+  //
+  // - `removing`: before server response
+  // - `removed`: after successfully removed
   
   remove: function (productId, fn, opts) {
     fn = fn || $.noop
@@ -110,6 +172,8 @@ Cart.prototype = {
     })
   },
   
+  // Wrap other function calls in this to make sure the cart is loaded
+  
   loadAndThen: function (fn) {
     if(this._loaded) {
       fn.call(this)
@@ -119,9 +183,13 @@ Cart.prototype = {
     return this
   },
   
+  // Is the cart empty?
+  // =====================
   isEmpty: function () {
     return !this.products || this.products.length < 1
   },
+  
+  // Update cart with server response
   
   update: function (cartData) {
     this._loaded = true
--- src/events.js
+++ src/events.js
@@ -1,3 +1,13 @@
+// Events module
+// =============
+// 
+// Wrap our own objects in jQuery's .bind() and .trigger() methods.
+//
+// Usage:
+//     var MyObject = {...}
+//     $.extend(MyObject, Events)
+//     MyObject.bind('foo', function (evt, i) { alert(i)})
+//     MyObject.trigger('foo', [1])
 var Events = {
   bind: function (eventName, fn) {
     $(this).bind(eventName, fn)
